94633e767106c85d76d3bf153e29ba184e8f6f43
Milhos <milhos.sihuay@gmail.com>
2024-11-18 06:54:28 -0500

    create ListaTablasBD

---
+++ app/src/main/java/com/example/myproyect/actividades/clases/ListaTablasBD.java
@@ -0,0 +1,10 @@
+package com.example.myproyect.actividades.clases;
+
+public class ListaTablasBD {
+
+    public static final String tabla1 = "reserva_losa1";
+    public static final String tabla2 = "reserva_losa2";
+    public static final String tabla3 = "reserva_losa3";
+    public static final String tabla4 = "reserva_losa4";
+
+}
